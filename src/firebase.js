6fc9fda4422292fcaaecbb984df5411dbc39e0c6
DFori <daniel.tatama@binghamuni.edu.ng>
2025-03-14 09:59:08 +0100

    hidden secrets

--- src/firebase.js
+++ src/firebase.js
@@ -3,20 +3,17 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
-
-// Import the functions you need from the SDKs you need
 import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB8qK71j496lpvD0LAtEd7IgT8EFZcDPBs",
-  authDomain: "port-gen.firebaseapp.com",
-  projectId: "port-gen",
-  storageBucket: "port-gen.firebasestorage.app",
-  messagingSenderId: "927545431578",
-  appId: "1:927545431578:web:976a5a9d261dc892d4a3a3",
-  measurementId: "G-4EWCWFLDSM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
